8ee16a09c025cc5a9974c1440ea2233eac28984f
juan ltv <juanmanueltarrago@gmail.com>
2021-04-21 18:19:12 -0300

    agrego el 4

---
+++ ej4.py
@@ -0,0 +1,6 @@
+#Dados como datos 200 números enteros, obtener y mostrar su suma.
+sum = 0
+for num in range(200):
+	dato = int(input("ingrese el numero:"))
+	sum = sum + dato
+print("la suma total es:",sum)
